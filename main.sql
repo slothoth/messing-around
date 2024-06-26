d8ad5a4ac7ed67dd08edb9517b84f889a209b94f
slothoth <smayobiz@gmail.com>
2024-06-26 04:21:14 +0100

    formations

--- main.sql
+++ main.sql
@@ -1,128 +1,136 @@
-INSERT INTO Types(Type, Kind) VALUES
-('HEROCLASS_WARRIOR', 'KIND_HEROCLASS'),
-('SLTH_UNIT_WARRIOR', 'KIND_UNIT'),
-('SLTH_UNIT_DUMMY', 'KIND_UNIT'),
-('ABILITY_SLTH_UNIT', 'KIND_ABILITY');
+-- shared values
+
+INSERT INTO RequirementSets(RequirementSetId, RequirementSetType) VALUES
+('SLTH_ON_SAME_TILE', 'REQUIREMENTSET_TEST_ALL');
+
+INSERT INTO RequirementSetRequirements(RequirementSetId, RequirementId) VALUES
+('SLTH_ON_SAME_TILE', 'SLTH_ON_SAME_TILE_REQ');
+
+INSERT INTO Requirements(RequirementId, RequirementType) VALUES
+('SLTH_ON_SAME_TILE_REQ', 'REQUIREMENT_PLOT_ADJACENT_TO_OWNER');
+
+INSERT INTO RequirementArguments(RequirementId, Name, Value) VALUES
+('SLTH_ON_SAME_TILE_REQ', 'MaxDistance', 0),
+('SLTH_ON_SAME_TILE_REQ', 'MinDistance', 0);
+
+--  first
+INSERT INTO Types(Type, Kind) VALUES ('FORMATION_CLASS_SLTH', 'KIND_FORMATION_CLASS'),
+                                     ('SLTH_UNIT', 'KIND_UNIT'),
+                                     ('ABILITY_SLTH_UNIT', 'KIND_ABILITY'),
+                                     ('ABILITY_SLTH_UNIT_ATTACH', 'KIND_ABILITY');
+
+INSERT INTO UnitFormationClasses(FormationClassType, Name) VALUES ('FORMATION_CLASS_SLTH', 'LOC_FORMATION_CLASS_SLTH');
+
+INSERT INTO Units(UnitType, IgnoreMoves, Stackable, BaseMoves, FormationClass, Description, Name, Combat, PurchaseYield, CanCapture, PromotionClass, Cost, BaseSightRange, Domain) VALUES
+('SLTH_UNIT', '0', '0', '1', 'FORMATION_CLASS_SLTH', 'LOC_DESCRIPTION', 'SEVEN_LEAGUE_BOOTS', '4', 'YIELD_GOLD', 1, 'PROMOTION_CLASS_RECON', '60', '2', 'DOMAIN_LAND');
+
+INSERT INTO UnitCaptures(CapturedUnitType, BecomesUnitType) VALUES
+('SLTH_UNIT', 'SLTH_UNIT');
 
 INSERT INTO TypeTags(Type, Tag) VALUES
-('SLTH_UNIT_DUMMY', 'CLASS_MELEE'),
-('SLTH_UNIT_DUMMY', 'CLASS_SLTH_UNIT'),
-('ABILITY_SLTH_UNIT', 'CLASS_SLTH_UNIT'),
-('SLTH_UNIT_WARRIOR', 'CLASS_MELEE');
+('SLTH_UNIT', 'CLASS_SLTH_UNIT'),
+('SLTH_UNIT', 'CLASS_SUPPORT'),
+('SLTH_UNIT', 'CLASS_ALL_ERAS'),
+('ABILITY_SLTH_UNIT_ATTACH', 'CLASS_SLTH_UNIT'),
+('ABILITY_SLTH_UNIT', 'CLASS_RECON'),
+('ABILITY_SLTH_UNIT', 'CLASS_MELEE'),
+('ABILITY_SLTH_UNIT', 'CLASS_RANGED'),
+('ABILITY_SLTH_UNIT', 'CLASS_ANTI_CAVALRY'),
+('ABILITY_SLTH_UNIT', 'CLASS_LIGHT_CAVALRY'),
+('ABILITY_SLTH_UNIT', 'CLASS_HEAVY_CAVALRY'),
+('ABILITY_SLTH_UNIT', 'CLASS_SIEGE'),
+('ABILITY_SLTH_UNIT', 'CLASS_NAVAL_MELEE'),
+('ABILITY_SLTH_UNIT', 'CLASS_NAVAL_RANGED'),
+('ABILITY_SLTH_UNIT', 'CLASS_NAVAL_RAIDER'),
+('ABILITY_SLTH_UNIT', 'CLASS_NAVAL_CARRIER'),
+('ABILITY_SLTH_UNIT', 'CLASS_AIRCRAFT');
 
 INSERT INTO Tags(Tag, Vocabulary) VALUES
 ('CLASS_SLTH_UNIT', 'ABILITY_CLASS');
 
-INSERT INTO Modifiers (ModifierId, ModifierType, RunOnce, Permanent) VALUES
-('SLTH_KILL_UNIT', 'MODIFIER_PLAYER_UNIT_ADJUST_DAMAGE', 1, 1);
-
-INSERT INTO ModifierArguments (ModifierId, Name, Value) VALUES
-('SLTH_KILL_UNIT', 'Amount', '100');
-
--- grant unit with experience: Fails by CTD: Maybe because not GP
-/*
-INSERT INTO Modifiers (ModifierId, ModifierType, RunOnce, Permanent) VALUES
-('SLTH_GET_UNIT', 'MODIFIER_PLAYER_UNIT_GRANT_UNIT_WITH_EXPERIENCE', 1, 1);
-
-INSERT INTO ModifierArguments (ModifierId, Name, Value) VALUES
-('SLTH_GET_UNIT', 'Experience', '1'),
-('SLTH_GET_UNIT', 'UnitType', 'SLTH_UNIT_WARRIOR'),
-
- */
-
--- grant unit in nearest city variant: does nothing as wrong context, goody hut
-/*
-INSERT INTO Modifiers (ModifierId, ModifierType, RunOnce, Permanent) VALUES
-('SLTH_GET_UNIT', 'MODIFIER_SINGLE_CITY_GRANT_UNIT_IN_NEAREST_CITY', 1, 1);
-
-INSERT INTO ModifierArguments (ModifierId, Name, Value) VALUES
-('SLTH_GET_UNIT', 'UnitType', 'SLTH_UNIT_WARRIOR'),
-('SLTH_GET_UNIT', 'Amount', '1');
-*/
-
--- grant unit in capital variant. Works, but not ideal, as not in same city
--- /*
-INSERT INTO Modifiers (ModifierId, ModifierType, RunOnce, Permanent) VALUES
-('SLTH_GET_UNIT', 'MODIFIER_PLAYER_GRANT_UNIT_IN_CAPITAL', 1, 1);
-
-INSERT INTO ModifierArguments (ModifierId, Name, Value) VALUES
-('SLTH_GET_UNIT', 'UnitType', 'SLTH_UNIT_WARRIOR'),
-('SLTH_GET_UNIT', 'Amount', '1'),
-('SLTH_GET_UNIT', 'AllowUniqueOverride', '0');
--- */
-
 INSERT INTO UnitAbilityModifiers(UnitAbilityType, ModifierId) VALUES
-('ABILITY_SLTH_UNIT', 'SLTH_GET_UNIT'),
-('ABILITY_SLTH_UNIT', 'SLTH_KILL_UNIT');
+('ABILITY_SLTH_UNIT_ATTACH', 'MODIFIER_ABILITY_SLTH_UNIT_ATTACH');
 
 INSERT INTO UnitAbilities(UnitAbilityType, Name, Description, Inactive) VALUES
-('ABILITY_SLTH_UNIT', 'LOC_ABILITY_SLTH_UNIT_NAME', 'LOC_ABABILITY_SLTH_UNIT_NAME', 1);
+('ABILITY_SLTH_UNIT_ATTACH', 'LOC_ABILITY_SLTH_UNIT_ATTACH_NAME', 'LOC_ABILITY_SLTH_UNIT_ATTACH_NAME', 0),
+('ABILITY_SLTH_UNIT', 'LOC_ABILITY_SLTH_UNIT_NAME', 'LOC_ABILITY_SLTH_UNIT_NAME', 1);
 
-INSERT INTO Units(UnitType, BaseMoves, FormationClass, AllowBarbarians, Description, Name, Combat, PromotionClass, Cost, BaseSightRange, Domain, PurchaseYield) VALUES
-('SLTH_UNIT_WARRIOR', '3', 'FORMATION_CLASS_LAND_COMBAT', 0, 'LOC_DESCRIPTION', 'Nightshade', 50, 'PROMOTION_CLASS_RECON', '60', '2', 'DOMAIN_LAND', 'YIELD_GOLD'),
-('SLTH_UNIT_DUMMY', '3', 'FORMATION_CLASS_LAND_COMBAT', 0, 'LOC_DESCRIPTION', 'Nightshade_Dummy', 5, 'PROMOTION_CLASS_RECON', '60', '2', 'DOMAIN_LAND', 'YIELD_GOLD');
+INSERT INTO Modifiers(ModifierId, ModifierType, SubjectRequirementSetId) VALUES
+('MODIFIER_ABILITY_SLTH_UNIT_ATTACH', 'MODIFIER_PLAYER_UNITS_GRANT_ABILITY', 'SLTH_ON_SAME_TILE');
 
-INSERT INTO HeroClasses (HeroClassType, Name, Description, UnitType, CreationProjectType, DiscoveryMinEraType) VALUES
-('HEROCLASS_WARRIOR', 'LOC_UNIT_HERO_WARRIOR_NAME', 'LOC_UNIT_HERO_WARRIOR_DESCRIPTION', 'SLTH_UNIT_WARRIOR', 'PROJECT_CREATE_HERO_HERCULES', 'ERA_FUTURE');
+INSERT INTO ModifierArguments(ModifierId, Name, Value) VALUES
+('MODIFIER_ABILITY_SLTH_UNIT_ATTACH', 'AbilityType', 'ABILITY_SLTH_UNIT');
 
-INSERT INTO HeroClassUnitCommands (HeroClassType, UnitCommandType, Passive) VALUES
-('HEROCLASS_WARRIOR', 'UNITCOMMAND_RESTORE_UNIT_MOVES', 0);
-
--- promotion workaround
-INSERT INTO UnitPromotionModifiers(UnitPromotionType, ModifierId) VALUES
-('PROMOTION_TORTOISE', 'SLTH_GET_UNIT');
-
--- inactive workaround
-/*
-INSERT INTO UnitAbilities(UnitAbilityType, Name, Description) VALUES
-('ABILITY_SLTH_UNIT_ACTIVATOR', 'LOC_ABILITY_SLTH_UNIT_NAMEs', 'LOC_ABABILITY_SLTH_UNIT_sNAME');
+-------------------------Actual Modifiers given by support units -------------------------------
 
 INSERT INTO UnitAbilityModifiers(UnitAbilityType, ModifierId) VALUES
-('ABILITY_SLTH_UNIT_ACTIVATOR', 'SLTH_ABILITY_ACTIVATE');
+('ABILITY_SLTH_UNIT', 'ESCORT_MOBILITY_SHARED_MOVEMENT'),
+('ABILITY_SLTH_UNIT', 'SLTH_MOVEMENT');
 
-INSERT INTO Modifiers(ModifierId, ModifierType, Permanent) VALUES
-('SLTH_ABILITY_ACTIVATE', 'MODIFIER_PLAYER_UNITS_GRANT_ABILITY', '1');
-
-INSERT INTO ModifierArguments(ModifierId, Name, "Value") VALUES
-('SLTH_ABILITY_ACTIVATE', 'AbilityType', 'ABILITY_SLTH_UNIT');
-
-INSERT INTO TypeTags(Type, Tag) VALUES
-('ABILITY_SLTH_UNIT_ACTIVATOR', 'CLASS_SLTH_UNIT');
-
-INSERT INTO Types(Type, Kind) VALUES
-('ABILITY_SLTH_UNIT_ACTIVATOR', 'KIND_ABILITY');
-
- */
-
--- Global grant workaround
-INSERT INTO GameModifiers(ModifierId) VALUES
-('SLTH_GRANT_ABILITY_ATTACH_PLAYERS');
+INSERT INTO ModifierArguments(ModifierId, Name, Type, Value) VALUES
+('SLTH_MOVEMENT', 'Amount', 'ARGTYPE_IDENTITY', '8');
 
 INSERT INTO Modifiers(ModifierId, ModifierType) VALUES
-('SLTH_GRANT_ABILITY_ATTACH_PLAYERS', 'MODIFIER_ALL_PLAYERS_ATTACH_MODIFIER');
+('SLTH_MOVEMENT', 'MODIFIER_PLAYER_UNIT_ADJUST_MOVEMENT');
 
-INSERT INTO ModifierArguments(ModifierId, Name, "Value") VALUES
-('SLTH_GRANT_ABILITY_ATTACH_PLAYERS', 'ModifierId', 'SLTH_ABILITY_ACTIVATE');
 
-INSERT INTO Modifiers(ModifierId, ModifierType, Permanent) VALUES
-('SLTH_ABILITY_ACTIVATE', 'MODIFIER_PLAYER_UNITS_GRANT_ABILITY', '1');
+-----   second equipment ---------------------------------------------------------------------------------------------
 
-INSERT INTO ModifierArguments(ModifierId, Name, "Value") VALUES
-('SLTH_ABILITY_ACTIVATE', 'AbilityType', 'ABILITY_SLTH_UNIT');
+INSERT INTO Types(Type, Kind) VALUES ('FORMATION_CLASS_SLTH_SWORD', 'KIND_FORMATION_CLASS'),
+                                     ('SLTH_UNIT_DAMAGE', 'KIND_UNIT'),
+                                     ('ABILITY_SLTH_UNIT_DAMAGE', 'KIND_ABILITY'),
+                                     ('ABILITY_SLTH_UNIT_DAMAGE_ATTACH', 'KIND_ABILITY');
 
-/*
-INSERT INTO BuildingModifiers(BuildingType, ModifierId) VALUES
-('BUILDING_PALACE', 'SLTH_GRANT_ABILITY_ATTACH_PLAYERS');
+INSERT INTO UnitFormationClasses(FormationClassType, Name) VALUES ('FORMATION_CLASS_SLTH_SWORD', 'LOC_FORMATION_CLASS_SLTH_SWORD');
 
-INSERT INTO Modifiers(ModifierId, ModifierType, SubjectStackLimit, OwnerStackLimit) VALUES
-('SLTH_GRANT_ABILITY_ATTACH_PLAYERS', 'MODIFIER_ALL_PLAYERS_ATTACH_MODIFIER', 1, 1);
+INSERT INTO Units(UnitType, IgnoreMoves, Stackable, BaseMoves, FormationClass, Description, Name, Combat, PurchaseYield, CanCapture, PromotionClass, Cost, BaseSightRange, Domain) VALUES
+('SLTH_UNIT_DAMAGE', '0', '0', '1', 'FORMATION_CLASS_SLTH_SWORD', 'LOC_SLTH_UNIT_DAMAGE_HUNTER_DESCRIPTION', 'SWORD_OF_STABBING', '4', 'YIELD_GOLD', 1, 'PROMOTION_CLASS_RECON', '60', '2', 'DOMAIN_LAND');
 
-INSERT INTO ModifierArguments(ModifierId, Name, "Value") VALUES
-('SLTH_GRANT_ABILITY_ATTACH_PLAYERS', 'AbilityType', 'SLTH_ABILITY_ACTIVATE');
+INSERT INTO UnitCaptures(CapturedUnitType, BecomesUnitType) VALUES
+('SLTH_UNIT_DAMAGE', 'SLTH_UNIT_DAMAGE');
 
-INSERT INTO Modifiers(ModifierId, ModifierType, Permanent) VALUES
-('SLTH_ABILITY_ACTIVATE', 'MODIFIER_PLAYER_UNITS_GRANT_ABILITY', '1');
+INSERT INTO TypeTags(Type, Tag) VALUES
+('SLTH_UNIT_DAMAGE', 'CLASS_SLTH_UNIT_DAMAGE'),
+('SLTH_UNIT_DAMAGE', 'CLASS_SUPPORT'),
+('SLTH_UNIT_DAMAGE', 'CLASS_ALL_ERAS'),
+('ABILITY_SLTH_UNIT_DAMAGE_ATTACH', 'CLASS_SLTH_UNIT_DAMAGE'),
+('ABILITY_SLTH_UNIT_DAMAGE', 'CLASS_RECON'),
+('ABILITY_SLTH_UNIT_DAMAGE', 'CLASS_MELEE'),
+('ABILITY_SLTH_UNIT_DAMAGE', 'CLASS_RANGED'),
+('ABILITY_SLTH_UNIT_DAMAGE', 'CLASS_ANTI_CAVALRY'),
+('ABILITY_SLTH_UNIT_DAMAGE', 'CLASS_LIGHT_CAVALRY'),
+('ABILITY_SLTH_UNIT_DAMAGE', 'CLASS_HEAVY_CAVALRY'),
+('ABILITY_SLTH_UNIT_DAMAGE', 'CLASS_SIEGE'),
+('ABILITY_SLTH_UNIT_DAMAGE', 'CLASS_NAVAL_MELEE'),
+('ABILITY_SLTH_UNIT_DAMAGE', 'CLASS_NAVAL_RANGED'),
+('ABILITY_SLTH_UNIT_DAMAGE', 'CLASS_NAVAL_RAIDER'),
+('ABILITY_SLTH_UNIT_DAMAGE', 'CLASS_NAVAL_CARRIER'),
+('ABILITY_SLTH_UNIT_DAMAGE', 'CLASS_AIRCRAFT');
 
-INSERT INTO ModifierArguments(ModifierId, Name, "Value") VALUES
-('SLTH_ABILITY_ACTIVATE', 'AbilityType', 'ABILITY_SLTH_UNIT');
- */
+INSERT INTO Tags(Tag, Vocabulary) VALUES
+('CLASS_SLTH_UNIT_DAMAGE', 'ABILITY_CLASS');
+
+INSERT INTO UnitAbilityModifiers(UnitAbilityType, ModifierId) VALUES
+('ABILITY_SLTH_UNIT_DAMAGE_ATTACH', 'MODIFIER_ABILITY_SLTH_UNIT_DAMAGE_ATTACH');
+
+INSERT INTO UnitAbilities(UnitAbilityType, Name, Description, Inactive) VALUES
+('ABILITY_SLTH_UNIT_DAMAGE_ATTACH', 'LOC_ABILITY_SLTH_UNIT_DAMAGE_ATTACH_NAME', 'LOC_ABILITY_SLTH_UNIT_DAMAGE_ATTACH_NAME', 0),
+('ABILITY_SLTH_UNIT_DAMAGE', 'LOC_ABILITY_SLTH_UNIT_DAMAGE_NAME', 'LOC_ABILITY_SLTH_UNIT_DAMAGE_NAME', 1);
+
+INSERT INTO Modifiers(ModifierId, ModifierType, SubjectRequirementSetId) VALUES
+('MODIFIER_ABILITY_SLTH_UNIT_DAMAGE_ATTACH', 'MODIFIER_PLAYER_UNITS_GRANT_ABILITY', 'SLTH_ON_SAME_TILE');
+
+INSERT INTO ModifierArguments(ModifierId, Name, Value) VALUES
+('MODIFIER_ABILITY_SLTH_UNIT_DAMAGE_ATTACH', 'AbilityType', 'ABILITY_SLTH_UNIT_DAMAGE');
+
+-------------------------Actual Modifiers given by support units -------------------------------
+
+INSERT INTO UnitAbilityModifiers(UnitAbilityType, ModifierId) VALUES
+('ABILITY_SLTH_UNIT_DAMAGE', 'ESCORT_MOBILITY_SHARED_MOVEMENT'),
+('ABILITY_SLTH_UNIT_DAMAGE', 'SLTH_SWORD_STRENGTH');
+
+INSERT INTO ModifierArguments(ModifierId, Name, Type, Value) VALUES
+('SLTH_SWORD_STRENGTH', 'Amount', 'ARGTYPE_IDENTITY', '10');
+
+INSERT INTO Modifiers(ModifierId, ModifierType) VALUES
+('SLTH_SWORD_STRENGTH', 'MODIFIER_UNIT_ADJUST_COMBAT_STRENGTH');
