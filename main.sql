0fedd0cbb09dbbf1afdeaa453b72c95eb6998071
slothoth <smayobiz@gmail.com>
2024-07-15 02:06:24 +0100

    lazy

--- main.sql
+++ main.sql
@@ -1,5 +1,5 @@
--- shared values
-INSERT INTO BuildingModifiers(BuildingType, ModifierId) VALUES ('BUILDING_GOV_CONQUEST', 'SLTH_GOV_CONQUEST_FLAG');
+-- tier 3 visibility requirements
+INSERT INTO RequirementSetRequirements(RequirementSetId, RequirementId) VALUES ('THIS_PLAYER_IS_DIFFERENT_SECRET_SOCIETY', 'REQUIRES_PLAYER_HAS_HIGH_DIPLO_VIS_OF_TARGET');
 
-INSERT INTO Modifiers(ModifierId, ModifierType) VALUES ('SLTH_GOV_CONQUEST_FLAG', 'MODIFIER_ATTACH');
-
+-- alt
+--DELETE from TraitModifiers WHERE ModifierId='STANDARD_DIPLOMATIC_DIFFERENT_SECRET_SOCIETY';
