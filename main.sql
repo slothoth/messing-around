b6baf830bac5c7b14b944c231fd79cd5d94d3f83
slothoth <smayobiz@gmail.com>
2024-07-15 02:09:47 +0100

    init

--- main.sql
+++ main.sql
@@ -1,5 +1,1 @@
--- tier 3 visibility requirements
-INSERT INTO RequirementSetRequirements(RequirementSetId, RequirementId) VALUES ('THIS_PLAYER_IS_DIFFERENT_SECRET_SOCIETY', 'REQUIRES_PLAYER_HAS_HIGH_DIPLO_VIS_OF_TARGET');
 
--- alt
---DELETE from TraitModifiers WHERE ModifierId='STANDARD_DIPLOMATIC_DIFFERENT_SECRET_SOCIETY';
