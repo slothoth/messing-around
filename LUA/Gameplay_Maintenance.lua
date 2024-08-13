373139fc0e4bb6c1943a52c3d2af96e832fddfa1
Mayo, Sam <sdm2000@hw.ac.uk>
2024-08-13 21:37:12 +0100

    xp_grant check

--- LUA/Gameplay_Maintenance.lua
+++ LUA/Gameplay_Maintenance.lua
@@ -1,193 +1,67 @@
--- Can we only trigger event when
--- any city is built
- --city builds a maintenance building,
- --civ changes in or out of maintenance policies?
--- gov plaza buiLt/destroyed, palace built/destroyed. hopefully recapital covers palace part. gov plaza unbuilt
--- CONSTANTS
-MaintenanceReductionBuildings = {SLTH_BUILDING_COURTHOUSE=-40};
-MaintenanceReductionPolicies = {POLICY_DISCIPLINE_HIDDEN={CityReduction=25, DistanceReduction=-50}, POLICY_GOD_KING_HIDDEN={CityReduction=-20, DistanceReduction=30}};
-GovernCentres = {SLTH_BUILDING_SUMMER_PALACE='summer', SLTH_BUILDING_WINTER_PALACE='winter'};
--- ('SLTH_BUILDING_GAMBLING_HOUSE', -10),
--- ('SLTH_BUILDING_TAVERN', 10),
--- ('SLTH_BUILDING_TAVERN_GRIGORI', 10),
--- ('SLTH_BUILDING_HARBOR_LANUN', -10),
--- ('SLTH_BUILDING_COURTHOUSE', -40),
--- ('SLTH_BUILDING_GOVERNORS_MANOR', -20),
--- ('SLTH_BUILDING_BASILICA', -40),
--- ('SLTH_BUILDING_TOWER_OF_COMPLACENCY', -50);
+local FreeXPUnits = { SLTH_UNIT_ADEPT = 1, SLTH_UNIT_IMP = 1, SLTH_UNIT_SHAMAN = 1, SLTH_UNIT_ARCHMAGE = 2, SLTH_UNIT_EATER_OF_DREAMS = 2,
+                      SLTH_UNIT_CORLINDALE = 2, SLTH_UNIT_DISCIPLE_OF_ACHERON = 1, SLTH_UNIT_GAELAN = 1.5, SLTH_UNIT_GIBBON = 2,
+                      SLTH_UNIT_GOVANNON = 2, SLTH_UNIT_HEMAH = 2, SLTH_UNIT_LICH = 2, SLTH_UNIT_ILLUSIONIST = 1.5, SLTH_UNIT_MAGE = 1.5,
+                      SLTH_UNIT_WIZARD = 1.5, SLTH_UNIT_MOBIUS_WITCH = 1.5, SLTH_UNIT_MOKKA = 1.5, SLTH_UNIT_SON_OF_THE_INFERNO = 2,
+                      UNIT_SLINGER = 1.5, UNIT_SCOUT = 1
+}
+-- last two to test
+-- SLTH_UNIT_MOKKA???
+function removeTest(playerID, unitID)
+    print("Unit removed: " .. unitID);
+end
 
--- ('SLTH_POLICY_DESPOTISM', 25, NULL),
--- ('SLTH_POLICY_CITY_STATES', -25, -80),
--- ('SLTH_POLICY_GOD_KING', 10, NULL),
--- ('SLTH_POLICY_ARISTOCRACY', -40, NULL),
+function FreePromotionFromResource(playerID, unitID)
+    print("Unit made: " .. unitID);
+    local pPlayer = Players[playerID];
+    local resources = pPlayer:GetResources()
+    print('Player resources table: ');
+    print(resources);
+    print('HasResources function works?:')
+    local hasResource = resources:HasResource();
+    print(hasResource);
+    print('GetResourceAmount function works?:')
+    local resourceAmount = resources:GetResourceAmount();
+    print(resourceAmount)
+    -- UI context functions HasExportedResource, GetBonusResourcePerTurn. Check if these work outside of strategics.
+    -- if Horses is index 43 or 44, its Resources table.
+    local pUnit = '';
+    pUnit:GetExperience():SetPromotion(1)           -- choose relevant promotion index
+end
 
-function MasterTax(playerId)
+function GrantXP(playerId)
     local pPlayer = Players[playerId];
-    if not pPlayer:IsMajor() then return; end;
-    local global_dist_reduction, global_city_reduction = CalculatePolicyReduction(pPlayer); -- currently broke as struggling to iterate over SQL table
-    local num_cities = 0;
-    local glob_dist_tax = 0.0;
-    local glob_num_tax = 0.0;
-    print('Player is: '.. playerId);
-    for idx, city in pPlayer:GetCities():Members() do
-        num_cities = num_cities + 1;
-    end
-    for idx, city in pPlayer:GetCities():Members() do
-        local dist_tax = city:GetProperty('distance');
-        local mult_city = city:GetProperty('city_mult');
-        local dist_tax_final = (dist_tax * mult_city) / 100 / 2;
-        local num_tax_final = (num_cities * mult_city) / 100    / 2;
-        glob_dist_tax = glob_dist_tax + dist_tax_final;
-        glob_num_tax = glob_num_tax + num_tax_final;
-        print('City Distance Tax cost:'.. dist_tax_final);
-        print('City Num Tax cost:'.. num_tax_final);
-    end
-    local num_tax_final = glob_num_tax * global_city_reduction / 100;
-    print('Global Num Cities Tax cost:'.. num_tax_final);
-    pPlayer:SetProperty('city_num_maintenance', num_tax_final);
-    local taxes = glob_dist_tax * global_dist_reduction / 100;
-    pPlayer:SetProperty('city_distance_maintenance', taxes);
-    print('Global Distance Cities Tax cost:'.. taxes);
-    taxes = taxes + num_tax_final;
-    print('Total tax cost:'.. -taxes);
-    pPlayer:GrantYield(2, -taxes);          -- 2 is hopefully gold attempt to index a number value?
-end
-
-function InitCityTax(playerID, cityID, x, y)
-    local pPlayer = Players[playerID];
-    local pCity = CityManager.GetCity(playerID, cityID);
-    local pCapital = pPlayer:GetCities():GetCapitalCity();        -- later on, do check if gov plaza closer
-    if pCapital then
-        local cap_x = pCapital:GetX();
-        local cap_y = pCapital:GetY();
-        local distance = Map.GetPlotDistance(x, y, cap_x, cap_y) - 3;
-        print('Distance from capital -3 on init: ' .. distance);
-        pCity:SetProperty('distance', distance);
-    else
-        -- print('No Capital found! Was this first city?');
-        pCity:SetProperty('distance', 0);
-    end
-    pCity:SetProperty('city_mult', 100);
-    pCity:SetProperty('city_mult', 100);
-end
-
-function BuildingTaxReductionMade(playerID, cityID, buildingID, plotID, isOriginalConstruction)
-    local buildingName = GameInfo.Buildings[buildingID];
-    print(buildingName.BuildingType);
-    local building_row = MaintenanceReductionBuildings[buildingName.BuildingType];
-    if building_row then
-        local pCity = CityManager.GetCity(playerID, cityID);
-	    local tax_val = pCity:GetProperty('city_mult');
-	    tax_val = tax_val + building_row;
-	    print('New tax mult on this city: ' .. tax_val);
-	    pCity:SetProperty('city_mult', tax_val);
-    end
-    local buildingGovern = GovernCentres[buildingName.BuildingType]
-    if buildingGovern then
-        local pPlayer = Players[playerID];
-        local pCity = CityManager.GetCity(playerID, cityID);
-        local location = {x=pCity:GetX(),y=pCity:GetY()}
-        pPlayer:SetProperty(buildingGovern, location);
-        local t_GovernorSeats = GetGovernanceDistances(pPlayer);
-        for _, city in pPlayer:GetCities():Members() do
-            CalculateGovernanceDistance(city, t_GovernorSeats);
-            print('city iterate-------------');
-        end
-    end
-end
-
-function BuildingTaxPillageStateChange(playerID, cityID, buildingID, isPillaged)
-    local buildingName = GameInfo.Buildings[buildingID];
-    local building_row = GameInfo.MaintenanceReductionBuildings[buildingName.BuildingType];     -- MaintenanceReductionBuildings[buildingName.BuildingType]
-    if building_row then
-        local pCity = CityManager.GetCity(playerID, cityID);
-        local tax_val = pCity:GetProperty('city_mult');
-    	if isPillaged then
-	        tax_val = tax_val - building_row;
-        else
-	        tax_val = tax_val + building_row;
-        end
-        pCity:SetProperty('city_mult', tax_val);
-    end
-    local buildingGovern = GovernCentres[buildingName.BuildingType]
-    if buildingGovern then
-        local pPlayer = Players[playerID];
-        local new_prop = nil;
-        if not isPillaged then
-	        new_prop = {x=pCity:GetX(), y=pCity:GetY()};
-        end
-        pPlayer:SetProperty(buildingGovern, new_prop);
-        local tGovernSeats = GetGovernanceDistances(pPlayer);
-        for _, city in pPlayer:GetCities():Members() do
-            CalculateGovernanceDistance(city, tGovernSeats);
-        end
-    end
-end
-
-function CapitalCityRecalc(playerID, cityID)
-    local pPlayer = Players[playerID];
-    local tGovernSeats = GetGovernanceDistances(pPlayer);
-    for _, city in pPlayer:GetCities():Members() do
-        CalculateGovernanceDistance(city, tGovernSeats);
-    end
-end
-
-function CalculatePolicyReduction(pPlayer)
-    local dist_mult = 100;
-    local city_mult = 100;
-    local kCulture = pPlayer:GetCulture();
-    for policyType, details in pairs(MaintenanceReductionPolicies) do
-        local policy = GameInfo.Policies[policyType];
-        if not policy then return 100, 100; end
-        if kCulture:IsPolicyActive(policy.Index) then
-            if details.CityReduction then
-                city_mult = city_mult + details.CityReduction;
-            end
-            if details.DistanceReduction then
-                dist_mult = dist_mult + details.DistanceReduction;
+    for i, unit in pPlayer:GetUnits():Members() do
+        local unitType = unit:GetUnitType();
+        local fXP_gain = FreeXPUnits[unitType]
+        if fXP_gain then
+            if fXP_gain == int(fXP_gain) then
+                -- if integer, simple add xp
+                unit:GetExperience():ChangeExperience(fXP_gain);
+            else
+                -- if float, use property to set state.
+                local iXP_portion, fXP_portion = math.modf(fXP_gain);
+                print('Decimal portion of xp gain is:')
+                print(fXP_portion);
+                local existing_xp_portion = unit:GetProperty('xp_portion');
+                if not existing_xp_portion then
+                    unit:SetProperty('xp_portion', fXP_portion);
+                    print('No prior xp_portion');
+                else
+                    local new_xp_portion = existing_xp_portion + fXP_portion;
+                    if new_xp_portion > 1 then
+                        iXP_portion = iXP_portion + 1;
+                        new_xp_portion = new_xp_portion - 1;
+                    end
+                    unit:SetProperty('xp_portion', new_xp_portion);
+                    print('Old xp_portion: ' .. existing_xp_portion .. ' New xp_portion: ' .. new_xp_portion);
+                end
+                print('Adding ' .. iXP_portion .. ' to unit.')
+                unit:GetExperience():ChangeExperience(iXP_portion);
             end
         end
     end
-    if dist_mult < 0 then dist_mult = 0; end
-    if city_mult < 0 then city_mult = 0; end
-    return dist_mult, city_mult
 end
 
-function CalculateGovernanceDistance(city, tGovernSeats_)
-    local city_x = city:GetX();
-    local city_y = city:GetY();
-    local best_distance = 999;
-    local closest_seat = '';
-    for building_name, location in pairs(tGovernSeats_) do
-        if location.empty then city:SetProperty('distance', best_distance); print('tGovernSeats_ was empty'); return; end     -- on first city settle
-        local distance = Map.GetPlotDistance(city_x, city_y, location.x, location.y);
-        if distance < best_distance then
-            best_distance = distance;
-            closest_seat = building_name;
-        end
-    end
-    print('distance is '.. best_distance .. ' from ' .. closest_seat);
-    city:SetProperty('distance', best_distance);
-end
-
-function GetGovernanceDistances(pPlayer)
-    local pCapital = pPlayer:GetCities():GetCapitalCity();
-    if not pCapital then local empty = {empty=True}; return {no_seat=empty}; end
-    local cap_x = pCapital:GetX();
-    local cap_y = pCapital:GetY();
-    local tGovernSeats = {};
-    table.insert(tGovernSeats, {x=cap_x, y=cap_y});
-    for building, property_name in pairs(GovernCentres) do
-        if pPlayer:GetProperty(property_name) then
-             tGovernSeats[building] = pPlayer:GetProperty(property_name);
-        end
-    end
-    return tGovernSeats
-end
-
-
-GameEvents.PlayerTurnStarted.Add(MasterTax);
-GameEvents.CityBuilt.Add(InitCityTax);
-GameEvents.BuildingConstructed.Add(BuildingTaxReductionMade);
-GameEvents.BuildingPillageStateChanged.Add(BuildingTaxPillageStateChange);
-GameEvents.CapitalCityChanged.Add(CapitalCityRecalc);
+GameEvents.PlayerTurnStarted.Add(GrantXP);
+GameEvents.UnitRemovedFromMap.Add(removeTest);
+GameEvents.UnitCreated.Add(FreePromotionFromResource);
