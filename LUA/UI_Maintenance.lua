85f0e60ec364bb7aad550055fb4a0a171461f3f7
Mayo, Sam <sdm2000@hw.ac.uk>
2024-08-13 21:55:44 +0100

    init

--- LUA/UI_Maintenance.lua
+++ LUA/UI_Maintenance.lua
@@ -1,12 +1,11 @@
 -- More ExposedMembers bindings for functions unavailable in gameplay scripts
 
 
-function Initialize()
-	if ExposedMembers.ExposedUnitPromotionsFunctions == nil then
-		ExposedMembers.ExposedUnitPromotionsFunctions = {}
-	end
-	ExposedMembers.ExposedUnitPromotionsFunctions.GetPromotions = GetPromotions;
-	ExposedMembers.ExposedUnitPromotionsFunctions.GivePromotion = GivePromotion;
-end
+--function Initialize()
+--	if ExposedMembers.UIMaintenance == nil then
+--		ExposedMembers.UIMaintenance = {}
+--	end
+--	ExposedMembers.UIMaintenance.GetPromotions = GetPromotions;
+-- end
 
-Initialize();
+--Initialize();
