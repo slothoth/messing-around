05690e0978977b57b14d291b22ac8dfe69ae1f1a
Mayo, Sam <sdm2000@hw.ac.uk>
2024-09-01 01:58:45 +0100

    plotProperty yields using binary properties

--- LUA/Gameplay_Maintenance.lua
+++ LUA/Gameplay_Maintenance.lua
@@ -1,193 +1,65 @@
--- Can we only trigger event when
--- any city is built
- --city builds a maintenance building,
- --civ changes in or out of maintenance policies?
--- gov plaza buiLt/destroyed, palace built/destroyed. hopefully recapital covers palace part. gov plaza unbuilt
--- CONSTANTS
-MaintenanceReductionBuildings = {SLTH_BUILDING_COURTHOUSE=-40};
-MaintenanceReductionPolicies = {POLICY_DISCIPLINE_HIDDEN={CityReduction=25, DistanceReduction=-50}, POLICY_GOD_KING_HIDDEN={CityReduction=-20, DistanceReduction=30}};
-GovernCentres = {SLTH_BUILDING_SUMMER_PALACE='summer', SLTH_BUILDING_WINTER_PALACE='winter'};
--- ('SLTH_BUILDING_GAMBLING_HOUSE', -10),
--- ('SLTH_BUILDING_TAVERN', 10),
--- ('SLTH_BUILDING_TAVERN_GRIGORI', 10),
--- ('SLTH_BUILDING_HARBOR_LANUN', -10),
--- ('SLTH_BUILDING_COURTHOUSE', -40),
--- ('SLTH_BUILDING_GOVERNORS_MANOR', -20),
--- ('SLTH_BUILDING_BASILICA', -40),
--- ('SLTH_BUILDING_TOWER_OF_COMPLACENCY', -50);
-
--- ('SLTH_POLICY_DESPOTISM', 25, NULL),
--- ('SLTH_POLICY_CITY_STATES', -25, -80),
--- ('SLTH_POLICY_GOD_KING', 10, NULL),
--- ('SLTH_POLICY_ARISTOCRACY', -40, NULL),
-
-function MasterTax(playerId)
-    local pPlayer = Players[playerId];
-    if not pPlayer:IsMajor() then return; end;
-    local global_dist_reduction, global_city_reduction = CalculatePolicyReduction(pPlayer); -- currently broke as struggling to iterate over SQL table
-    local num_cities = 0;
-    local glob_dist_tax = 0.0;
-    local glob_num_tax = 0.0;
-    print('Player is: '.. playerId);
-    for idx, city in pPlayer:GetCities():Members() do
-        num_cities = num_cities + 1;
+function preCalcBinaryMap(iMax, mapMap)
+    for var=0,15 do
+        mapMap[var] = NumberToBinaryTable(var, iMax)
     end
-    for idx, city in pPlayer:GetCities():Members() do
-        local dist_tax = city:GetProperty('distance');
-        local mult_city = city:GetProperty('city_mult');
-        local dist_tax_final = (dist_tax * mult_city) / 100 / 2;
-        local num_tax_final = (num_cities * mult_city) / 100    / 2;
-        glob_dist_tax = glob_dist_tax + dist_tax_final;
-        glob_num_tax = glob_num_tax + num_tax_final;
-        print('City Distance Tax cost:'.. dist_tax_final);
-        print('City Num Tax cost:'.. num_tax_final);
-    end
-    local num_tax_final = glob_num_tax * global_city_reduction / 100;
-    print('Global Num Cities Tax cost:'.. num_tax_final);
-    pPlayer:SetProperty('city_num_maintenance', num_tax_final);
-    local taxes = glob_dist_tax * global_dist_reduction / 100;
-    pPlayer:SetProperty('city_distance_maintenance', taxes);
-    print('Global Distance Cities Tax cost:'.. taxes);
-    taxes = taxes + num_tax_final;
-    print('Total tax cost:'.. -taxes);
-    pPlayer:GrantYield(2, -taxes);          -- 2 is hopefully gold attempt to index a number value?
+    return mapMap
 end
 
-function InitCityTax(playerID, cityID, x, y)
-    local pPlayer = Players[playerID];
-    local pCity = CityManager.GetCity(playerID, cityID);
-    local pCapital = pPlayer:GetCities():GetCapitalCity();        -- later on, do check if gov plaza closer
-    if pCapital then
-        local cap_x = pCapital:GetX();
-        local cap_y = pCapital:GetY();
-        local distance = Map.GetPlotDistance(x, y, cap_x, cap_y) - 3;
-        print('Distance from capital -3 on init: ' .. distance);
-        pCity:SetProperty('distance', distance);
-    else
-        -- print('No Capital found! Was this first city?');
-        pCity:SetProperty('distance', 0);
+function SetBinaryProperty(pObject, sPropertyTemplate, iNum)
+    local tBinaryMap = g_tBinaryMapSet[iNum]
+    for iBitVal, iBit in pairs(tBinaryMap) do
+        local sProperty = string.format(sPropertyTemplate, iBitVal)
+        print(sProperty, iBit)
+        pObject:SetProperty(sProperty, iBit)
     end
-    pCity:SetProperty('city_mult', 100);
-    pCity:SetProperty('city_mult', 100);
 end
 
-function BuildingTaxReductionMade(playerID, cityID, buildingID, plotID, isOriginalConstruction)
-    local buildingName = GameInfo.Buildings[buildingID];
-    print(buildingName.BuildingType);
-    local building_row = MaintenanceReductionBuildings[buildingName.BuildingType];
-    if building_row then
-        local pCity = CityManager.GetCity(playerID, cityID);
-	    local tax_val = pCity:GetProperty('city_mult');
-	    tax_val = tax_val + building_row;
-	    print('New tax mult on this city: ' .. tax_val);
-	    pCity:SetProperty('city_mult', tax_val);
-    end
-    local buildingGovern = GovernCentres[buildingName.BuildingType]
-    if buildingGovern then
-        local pPlayer = Players[playerID];
-        local pCity = CityManager.GetCity(playerID, cityID);
-        local location = {x=pCity:GetX(),y=pCity:GetY()}
-        pPlayer:SetProperty(buildingGovern, location);
-        local t_GovernorSeats = GetGovernanceDistances(pPlayer);
-        for _, city in pPlayer:GetCities():Members() do
-            CalculateGovernanceDistance(city, t_GovernorSeats);
-            print('city iterate-------------');
+function NumberToBinaryTable(iNum, iMax)                                -- taken from Suktritact al Andaluz
+    assert(iNum >= 0, "number must be positive value")
+    assert(iMax >= 0, "maximum must be positive value")
+
+    iNum = math.floor(iNum) -- integers only!
+    iNum = math.min(iNum, iMax)
+
+    local iNumDigits = math.floor(math.log(iMax)/math.log(2)) + 1
+    local tBinaryMap 	= {}
+    for i = iNumDigits-1, 0, -1 do tBinaryMap[2^(i)] = 0 end
+    local iCurPow		= 0
+    ---------------------------
+    repeat
+        local iBitNum	= iNum % 2
+        iNum			= (iNum - iBitNum)/2
+
+        tBinaryMap[2^(iCurPow)] = iBitNum
+        iCurPow = iCurPow + 1
+    until (iNum == 0)
+    ---------------------------
+    return tBinaryMap
+end
+
+function AddYields (type, severity, plotx, ploty, mitigationLevel, randomEventID, gameCorePlaybackEventID)
+    print('Disaster! of type');
+    print(type)
+    if type ~= 2 then return end          -- check if correct diaster, volcano
+    local iBonus = severity + 0
+    for direction = 0, DirectionTypes.NUM_DIRECTION_TYPES - 1, 1 do     -- shamelessly stolen from LeeS
+        local pAdjacentPlot = Map.GetAdjacentPlot(plotx, ploty, direction)
+        if pAdjacentPlot then
+            SetBinaryProperty(pPlot, 'SLTH_VOLCANO_', iBonus)
         end
     end
 end
 
-function BuildingTaxPillageStateChange(playerID, cityID, buildingID, isPillaged)
-    local buildingName = GameInfo.Buildings[buildingID];
-    local building_row = GameInfo.MaintenanceReductionBuildings[buildingName.BuildingType];     -- MaintenanceReductionBuildings[buildingName.BuildingType]
-    if building_row then
-        local pCity = CityManager.GetCity(playerID, cityID);
-        local tax_val = pCity:GetProperty('city_mult');
-    	if isPillaged then
-	        tax_val = tax_val - building_row;
-        else
-	        tax_val = tax_val + building_row;
+local bBillCivPresent
+local sCivType
+for k, v in ipairs(PlayerManager.GetWasEverAliveIDs()) do
+    if not bBillCivPresent then
+		sCivType = PlayerConfigurations[v]:GetCivilizationTypeName()
+        if sCivType == 'CIVILIZATION_ROME' then
+            bBillCivPresent = true
+            g_tBinaryMapSet = {}
+            g_tBinaryMapSet = preCalcBinaryMap(iMax, tBinaryMapSet)
+            Events.RandomEventOccurred.Add(AddYields);
         end
-        pCity:SetProperty('city_mult', tax_val);
-    end
-    local buildingGovern = GovernCentres[buildingName.BuildingType]
-    if buildingGovern then
-        local pPlayer = Players[playerID];
-        local new_prop = nil;
-        if not isPillaged then
-	        new_prop = {x=pCity:GetX(), y=pCity:GetY()};
-        end
-        pPlayer:SetProperty(buildingGovern, new_prop);
-        local tGovernSeats = GetGovernanceDistances(pPlayer);
-        for _, city in pPlayer:GetCities():Members() do
-            CalculateGovernanceDistance(city, tGovernSeats);
-        end
-    end
+	end
 end
-
-function CapitalCityRecalc(playerID, cityID)
-    local pPlayer = Players[playerID];
-    local tGovernSeats = GetGovernanceDistances(pPlayer);
-    for _, city in pPlayer:GetCities():Members() do
-        CalculateGovernanceDistance(city, tGovernSeats);
-    end
-end
-
-function CalculatePolicyReduction(pPlayer)
-    local dist_mult = 100;
-    local city_mult = 100;
-    local kCulture = pPlayer:GetCulture();
-    for policyType, details in pairs(MaintenanceReductionPolicies) do
-        local policy = GameInfo.Policies[policyType];
-        if not policy then return 100, 100; end
-        if kCulture:IsPolicyActive(policy.Index) then
-            if details.CityReduction then
-                city_mult = city_mult + details.CityReduction;
-            end
-            if details.DistanceReduction then
-                dist_mult = dist_mult + details.DistanceReduction;
-            end
-        end
-    end
-    if dist_mult < 0 then dist_mult = 0; end
-    if city_mult < 0 then city_mult = 0; end
-    return dist_mult, city_mult
-end
-
-function CalculateGovernanceDistance(city, tGovernSeats_)
-    local city_x = city:GetX();
-    local city_y = city:GetY();
-    local best_distance = 999;
-    local closest_seat = '';
-    for building_name, location in pairs(tGovernSeats_) do
-        if location.empty then city:SetProperty('distance', best_distance); print('tGovernSeats_ was empty'); return; end     -- on first city settle
-        local distance = Map.GetPlotDistance(city_x, city_y, location.x, location.y);
-        if distance < best_distance then
-            best_distance = distance;
-            closest_seat = building_name;
-        end
-    end
-    print('distance is '.. best_distance .. ' from ' .. closest_seat);
-    city:SetProperty('distance', best_distance);
-end
-
-function GetGovernanceDistances(pPlayer)
-    local pCapital = pPlayer:GetCities():GetCapitalCity();
-    if not pCapital then local empty = {empty=True}; return {no_seat=empty}; end
-    local cap_x = pCapital:GetX();
-    local cap_y = pCapital:GetY();
-    local tGovernSeats = {};
-    table.insert(tGovernSeats, {x=cap_x, y=cap_y});
-    for building, property_name in pairs(GovernCentres) do
-        if pPlayer:GetProperty(property_name) then
-             tGovernSeats[building] = pPlayer:GetProperty(property_name);
-        end
-    end
-    return tGovernSeats
-end
-
-
-GameEvents.PlayerTurnStarted.Add(MasterTax);
-GameEvents.CityBuilt.Add(InitCityTax);
-GameEvents.BuildingConstructed.Add(BuildingTaxReductionMade);
-GameEvents.BuildingPillageStateChanged.Add(BuildingTaxPillageStateChange);
-GameEvents.CapitalCityChanged.Add(CapitalCityRecalc);
